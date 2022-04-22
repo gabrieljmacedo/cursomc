9deabdc72e37b38e1054f02efffd9138b70824a4
gabrieljmacedo <gabrieljm58@hotmail.com>
2022-04-21 22:41:32 -0300

    teste da primeira classe de dominio -categoria

--- src/main/java/com/br/gabriel/cursomc/domain/Categoria.java
+++ src/main/java/com/br/gabriel/cursomc/domain/Categoria.java
@@ -1,2 +1,54 @@
-package com.br.gabriel.cursomc.domain;public class Categoria {
+package com.br.gabriel.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // attributes
+    private Integer id;
+    private String nome;
+
+    // constructor
+    public Categoria(){
+
+    }
+
+    public Categoria(Integer id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    //gt + st
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // hash & Equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }

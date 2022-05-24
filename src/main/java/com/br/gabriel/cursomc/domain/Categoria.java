0af0fcfd6da81fa21333c584e1850959fd64c6b1
gabrieljmacedo <gabrieljm58@hotmail.com>
2022-05-24 14:13:45 -0300

    implementação na classe Categoria

--- src/main/java/com/br/gabriel/cursomc/domain/Categoria.java
+++ src/main/java/com/br/gabriel/cursomc/domain/Categoria.java
@@ -1,19 +1,16 @@
 package com.br.gabriel.cursomc.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
-@Entity
+@Entity(name = "CATEGORIA")
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
     // attributes
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 

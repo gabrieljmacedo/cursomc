ba1cdc57dcdb00d7247f048f2eb3f8359eacd9b5
gabrieljmacedo <gabrieljm58@hotmail.com>
2022-06-07 23:27:35 -0300

    estado e cidade inclusos

---
+++ src/main/java/com/br/gabriel/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.br.gabriel.cursomc.repositories;
+
+import com.br.gabriel.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}

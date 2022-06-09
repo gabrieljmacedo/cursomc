66db46e62f80f509a542b9d8185404baccbcf768
gabrieljmacedo <gabrieljm58@hotmail.com>
2022-06-09 17:47:13 -0300

    endpoint /clientes/{id} disponível

--- src/main/java/com/br/gabriel/cursomc/domain/Cliente.java
+++ src/main/java/com/br/gabriel/cursomc/domain/Cliente.java
@@ -1,6 +1,7 @@
 package com.br.gabriel.cursomc.domain;
 
 import com.br.gabriel.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -22,6 +23,7 @@ public class Cliente implements Serializable {
 
     private Integer tipo;
 
+    @JsonManagedReference
     @OneToMany(mappedBy = "cliente")
     private List<Endereco> enderecos = new ArrayList<>();
 

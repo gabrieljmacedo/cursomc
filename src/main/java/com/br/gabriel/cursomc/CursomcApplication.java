5f1d9b8f85fa0599d5d4fedcc1722ce2ff322aed
gabrieljmacedo <gabrieljm58@hotmail.com>
2023-01-17 09:13:51 -0300

    finalização da implementação itempedido e itempedidopk

--- src/main/java/com/br/gabriel/cursomc/CursomcApplication.java
+++ src/main/java/com/br/gabriel/cursomc/CursomcApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -39,6 +40,9 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepositoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -110,5 +114,18 @@ public class CursomcApplication implements CommandLineRunner {
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepositoryRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
--- src/main/java/com/br/gabriel/cursomc/domain/ItemPedido.java
+++ src/main/java/com/br/gabriel/cursomc/domain/ItemPedido.java
@@ -1,11 +1,15 @@
 package com.br.gabriel.cursomc.domain;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class ItemPedido implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @EmbeddedId
     private ItemPedidoPK id = new ItemPedidoPK();
     private Double desconto;
     private Integer quantidade;

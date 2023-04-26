b9afda4b568e4c0e60b620bf12fafcf65933d2fd
gabrieljmacedo <gabrieljm58@hotmail.com>
2023-04-26 16:25:55 -0300

    criação de validação customizada: e-mail não repetido na inclusão do cliente

--- src/main/java/com/br/gabriel/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/br/gabriel/cursomc/repositories/ClienteRepository.java
@@ -3,8 +3,12 @@ package com.br.gabriel.cursomc.repositories;
 import com.br.gabriel.cursomc.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 
 }
--- src/main/java/com/br/gabriel/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/br/gabriel/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.br.gabriel.cursomc.services.validation;
 
+import com.br.gabriel.cursomc.domain.Cliente;
 import com.br.gabriel.cursomc.domain.enums.TipoCliente;
 import com.br.gabriel.cursomc.dto.ClienteNewDTO;
+import com.br.gabriel.cursomc.repositories.ClienteRepository;
 import com.br.gabriel.cursomc.resources.exception.FieldMessage;
 import com.br.gabriel.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +14,10 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -25,6 +32,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         }
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido!"));
+        }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "E-mail já existente!"));
         }
 
         // inclua os testes aqui, inserindo erros na lista

9261c362d5e42d8720792861fdd675562dcdb935
gabrieljmacedo <gabrieljm58@hotmail.com>
2023-04-27 09:21:52 -0300

    validação customizada: validação de e-mail não repetido na inclusão de cliente

--- src/main/java/com/br/gabriel/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/br/gabriel/cursomc/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.br.gabriel.cursomc.dto;
 
 import com.br.gabriel.cursomc.domain.Cliente;
+import com.br.gabriel.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
